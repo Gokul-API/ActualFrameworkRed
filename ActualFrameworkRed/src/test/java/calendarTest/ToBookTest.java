92333d0fa222993677664d6ce26d6db27864870e
Admin <Admin@DESKTOP-HSPIA4U>
2025-06-25 20:15:01 +0530

    Written contact and organization testscript

---
+++ ActualFrameworkRed/src/test/java/calendarTest/ToBookTest.java
@@ -0,0 +1,10 @@
+package calendarTest;
+
+public class ToBookTest {
+
+	public static void main(String[] args) {
+		System.out.println("Calendar created by santhosh");
+
+	}
+
+}
--- ActualFrameworkRed/src/test/java/contactTest/ToCreateContactTest.java
+++ ActualFrameworkRed/src/test/java/contactTest/ToCreateContactTest.java
@@ -3,7 +3,8 @@ package contactTest;
 public class ToCreateContactTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("I create contact");
+		System.out.println("Contact create by santhosh");
 
 	}
 
